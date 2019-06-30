47933dd20891a45849a27fc4400f212225872cf4
Tien Pham <tiendev94@gmail.com>
2019-06-30 20:36:05 +0300

    scaffold

---
+++ build/index.js
@@ -0,0 +1,12 @@
+"use strict";
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
