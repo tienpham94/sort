26422a9d3d836a330399c8f9ad0a90b9bdc4df7a
Tien Pham <tiendev94@gmail.com>
2019-07-01 13:38:08 +0300

    match

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
-import { Sorter } from './Sorter';
-import { NumbersCollection } from './NumbersCollection';
-import { CharactersCollection } from './CharactersCollection';
+import fs from 'fs'
 
-// const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
-// console.log(numbersCollection.data);
+const matches = fs.readFileSync('football.csv', {
+  encoding: 'utf-8'
+}).split('\n').map((row:string):string[] => row.split(','))
 
-const charactersCollection = new CharactersCollection('Xaayb');
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
-console.log(charactersCollection.data);
+console.log(matches)
