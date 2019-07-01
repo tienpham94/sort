473e361a02ddae5e276379ad957703863945219a
Tien Pham <tiendev94@gmail.com>
2019-07-01 14:31:20 +0300

    convert

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,4 +1,7 @@
-import fs from 'fs';
+import fs from "fs";
+
+import { dateStringToDate } from "./utils";
+import { MatchResult } from "./MatchResult";
 
 export class CsvFileReader {
   data: string[][] = [];
@@ -6,15 +9,28 @@ export class CsvFileReader {
   constructor(public filename: string) {}
 
   read(): void {
-   this.data = fs
-     .readFileSync(this.filename, {
-       encoding: 'utf-8'
-     })
-     .split('\n')
-     .map(
-       (row: string): string[] => {
-         return row.split(',');
-       }
-     );
- }
+    this.data = fs
+      .readFileSync(this.filename, {
+        encoding: "utf-8"
+      })
+      .split("\n")
+      .map(
+        (row: string): string[] => {
+          return row.split(",");
+        }
+      )
+      .map(
+        (row: string[]): any => {
+          return [
+            dateStringToDate(row[0]),
+            row[1],
+            row[2],
+            parseInt(row[3]),
+            parseInt(row[4]),
+            row[5] as MatchResult,
+            row[6]
+          ];
+        }
+      );
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,19 @@
-import { CsvFileReader } from './CsvFileReader';
+import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./MatchResult";
 
-const reader = new CsvFileReader('football.csv');
+const reader = new CsvFileReader("football.csv");
 reader.read();
 
-// enum - enumeration
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D'
-}
+console.log(reader.data);
 
 let manUnitedWins = 0;
 
- for (let match of reader.data) {
-  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+for (let match of reader.data) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }
 
- console.log(`Man United won ${manUnitedWins} games`);
+console.log(`Man United won ${manUnitedWins} games`);
